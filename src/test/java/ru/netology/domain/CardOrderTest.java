26826288b7402d401594273d8194fffb55bb4f02
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2021-03-20 17:15:46 +0300

    Initial commit

---
+++ src/test/java/ru/netology/domain/CardOrderTest.java
@@ -0,0 +1,35 @@
+package ru.netology.domain;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+    @Test
+    void getTrueInputCardDeliveryAppTest(){
+        open("http://localhost:9999");
+        $("[data-test-id = 'city'] input").setValue("Москва");
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 10);
+        SimpleDateFormat formDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String chooseDate = formDateFormat.format(calendar.getTime());
+        SelenideElement dateElement =  $("[data-test-id=date] input[class=input__control]");
+        dateElement.sendKeys("\b\b\b\b\b\b\b\b\b\b");
+        dateElement.setValue(chooseDate);
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+71234567891");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=notification]")
+                .$(withText("Встреча успешно забронирована на"))
+                .waitUntil(visible, 15000);
+
+    }
+}
