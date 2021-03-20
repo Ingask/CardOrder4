648f47ab28b8bd5d51378d217f5ccd76c23939be
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2021-03-20 17:24:40 +0300

    Test with a mistake

--- src/test/java/ru/netology/domain/CardOrderTest.java
+++ src/test/java/ru/netology/domain/CardOrderTest.java
@@ -24,7 +24,7 @@ public class CardOrderTest {
         dateElement.sendKeys("\b\b\b\b\b\b\b\b\b\b");
         dateElement.setValue(chooseDate);
         $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("+71234567891");
+        $("[data-test-id=phone] input").setValue("+7123456789");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id=notification]")
